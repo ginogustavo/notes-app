3faa61424c1af5c76c4654a19b356fc1e6d893b3
Gino Ureta <ing.gino@gmail.com>
2021-11-09 07:02:49 -0500

    Importing 3rd party modules

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 //const firstName = require("./utils");
-
 //const add = require("./utils");
-
 const getNotes = require("./notes");
+const validator = require("validator");
+
+console.log(validator.isEmail("gino@gmail.com")); //true
+console.log(validator.isEmail("gmail.com")); //false
+
+console.log(validator.isURL("https://github.com")); //true
+console.log(validator.isURL("https:/github.com")); //false
 
 // const fs = require("fs");
 // fs.writeFileSync("notes.txt", "Author: Gino.");
@@ -21,3 +26,17 @@ const getNotes = require("./notes");
 
 const message = getNotes();
 console.log(message);
+
+// 1. install the validator.js module
+// $ npm i validator@10.8.0
+// - it updates package.json by adding dependency
+// - it creates package-lock.json (extra info making npm a bit faster and secure)
+// - it creates node_modules directory with the libraries
+
+/**
+ *  Extra info in package-lock.json if we were to install the
+ *  library again, maintained by npm :
+ *    "version": "10.8.0",    --> exact version we installed
+ *    "resolved": "http//re", --> where it was fetched
+ *    "integrity": "sha52-mXX --> sha# making sure we are getting the exact code
+ */
